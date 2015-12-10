112cdfd7c4754508f0c085cb1252b12464da7ed5
Pablite5 <Pablite5@GAMING>
2015-12-10 08:55:15 +0100

    Cambio de variables

--- Examen Contornos/src/examen/contornos/ExamenContornos.java
+++ Examen Contornos/src/examen/contornos/ExamenContornos.java
@@ -21,30 +21,30 @@ public class ExamenContornos {
     public static boolean p = false;
 
     public static void main(String arg[]) {
-        int dig = 3;
-        int ndig = 0;
-        if (dig <= 0) {
+        int digito = 3;
+        int numerodigitos = 0;
+        if (digito <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
         for (int i = 1; i <= 99999; i++) {
             int aux = i;
 
-            int contador = 0;
+            int contadorx = 0;
 
             while (aux != 0) {
                 aux = aux / 10;
-                contador++;
+                contadorx++;
             }
-            ndig = contador;
+            numerodigitos = contadorx;
 
-            if (ndig == dig) {
+            if (numerodigitos == digito) {
                 if (i < 4) {
                     p = true;
                 } else {
                     if (i % 2 == 0) {
                         p = false;
                     } else {
-                        int contador1 = 0;
+                        int contadorx1 = 0;
                         int i1 = 1;
                         int k = (i - 1) / 2;
                         if (k % 2 == 0) {
@@ -53,15 +53,15 @@ public class ExamenContornos {
 
                         while (i1 <= k) {
                             if (i % i1 == 0) {
-                                contador1++;
+                                contadorx1++;
                             }
                             i1 += 2;
-                            if (contador1 == 2) {
+                            if (contadorx1 == 2) {
                                 i1 = k + 1;
                             }
                         }
 
-                        if (contador1 == 1) {
+                        if (contadorx1 == 1) {
                             p = true;
                         }
                     }
